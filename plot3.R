df58ffff51a273a2900a9cb210a08715772d2442
Antonio Rodriguez <taserian@gmail.com>
2015-05-09 23:12:46 -0400

    reduced size of legend text

--- plot3.R
+++ plot3.R
@@ -2,7 +2,8 @@
 datafile <- file("household_power_consumption.txt")
 
 # Using grep to extract the lines from the datafile which start with either "Date", "1/2/2007" or "2/2/2007"
-powerCons <- read.table(text = grep("(^[1,2]/2/2007|^Date)", readLines(datafile), value = TRUE), sep=";", header=TRUE, na.strings="?")
+powerCons <- read.table(text = grep("(^[1,2]/2/2007|^Date)", 
+                readLines(datafile), value = TRUE), sep=";", header=TRUE, na.strings="?")
 
 # Convert date to a true Date format 
 powerCons$Date <- as.Date(powerCons$Date, format = "%d/%m/%Y")
@@ -14,7 +15,8 @@ with(powerCons, {
   lines(Sub_metering_2 ~ DateTime, col="red")
   lines(Sub_metering_3 ~ DateTime, col="blue")
 })
-legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, legend=c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, cex=0.75, 
+       legend=c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
 
 # I think it looks good, so let's save it as a .png
 dev.copy(png, file="plot3.png")
